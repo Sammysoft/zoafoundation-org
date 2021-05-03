692aa369e89fc47066b4fef2a2fafff07b3b1131
Dare-Owonibi <dareowonibisamuel@gmail.com>
2021-05-03 13:06:25 +0100

    making some adjustments

--- src/Components/Pages/Team/team.js
+++ src/Components/Pages/Team/team.js
@@ -27,10 +27,10 @@ function Profile(props) {
                                     </a>
                                 </ul>
                             </div>
-                            <h1>Arijeloye Olakitan zacchaeus</h1>
+                            <h1 style={{fontWeight: 'bolder'}}>Arijeloye Olakitan zacchaeus</h1>
                             <h2>Founder</h2>
                             <hr />
-                            <p class="text-justify">Arijeloye Olakitan the founder of zoa foundation initiatives hails from Ibuji ondo state, born on the 10th of May, 1993 with a BSc degree in Statistics.</p>
+                            <p class="text-justify">Arijeloye Olakitan the founder of zoa foundation initiatives hails from Ibuji ondo state, born on the 10th of May, with a BSc degree in Statistics.</p>
                         </div>
                         <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 team">
                             <div class="img-box">
@@ -47,110 +47,10 @@ function Profile(props) {
                                     </a>
                                 </ul>
                             </div>
-                            <h1>Alofe Oluwatoba Andrew</h1>
+                            <h1 style={{fontWeight: 'bolder'}}>Alofe Oluwatoba Andrew</h1>
                             <h2>Coordinator</h2>
                             <hr />
-                            <p class="text-justify">Alofe Oluwatoba Andrew is the coordinator of this great foundation, with great passion and industriious efforts towards the operational efficacy of this organization</p>
-                        </div>
-                        <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 team">
-                            <div class="img-box">
-                                <img class="img-responsive" alt="Responsive Team Profiles" src="assets/img/team/Awolegon_Rachael.jpeg" />
-                                <ul class="text-center">
-                                    <a href="#">
-                                        <li><i className="bx bxl-facebook"></i></li>
-                                    </a>
-                                    <a href="#">
-                                        <li><i className="bx bxl-twitter"></i></li>
-                                    </a>
-                                    <a href="#">
-                                        <li><i className="bx bxl-linkedin"></i></li>
-                                    </a>
-                                </ul>
-                            </div>
-                            <h1>Awolegan Rachael Oluwatobiloba</h1>
-                            <h2>pioneer member</h2>
-                            <hr />
-                            <p class="text-justify">Miss Rachael Awolegan is a pioneer member of this great foundation, she is a fashion designer from ondo state, graduated from the university with a degree in Environmental health technology, born on the 10th of May 1996.</p>
-                        </div>
-                        <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 team">
-                            <div class="img-box">
-                                <img class="img-responsive" alt="Responsive Team Profiles" src="assets/img/team/Akinyemi_Henry.jpeg" />
-                                <ul class="text-center">
-                                    <a href="#">
-                                        <li><i className="bx bxl-facebook"></i></li>
-                                    </a>
-                                    <a href="#">
-                                        <li><i className="bx bxl-twitter"></i></li>
-                                    </a>
-                                    <a href="#">
-                                        <li><i className="bx bxl-linkedin"></i></li>
-                                    </a>
-                                </ul>
-                            </div>
-                            <h1>Akinyemi Henry Olamide</h1>
-                            <h2>pioneer member</h2>
-                            <hr />
-                            <p class="text-justify">Master Akinyemi Henry is a self employed citizen of Ekiti state with a degree in Business Education</p>
-                        </div>
-                        <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 team">
-                            <div class="img-box">
-                                <img class="img-responsive" alt="Responsive Team Profiles" src="assets/img/team/Olakitan_silifat.jpeg" />
-                                <ul class="text-center">
-                                    <a href="#">
-                                        <li><i className="bx bxl-facebook"></i></li>
-                                    </a>
-                                    <a href="#">
-                                        <li><i className="bx bxl-twitter"></i></li>
-                                    </a>
-                                    <a href="#">
-                                        <li><i className="bx bxl-linkedin"></i></li>
-                                    </a>
-                                </ul>
-                            </div>
-                            <h1>Ayodele Olakiitan Silifat</h1>
-                            <h2>Pioneer member</h2>
-                            <hr />
-                            <p class="text-justify">Olakiitan Silifat hails from Ekiti state, born on the 12th of June 1993</p>
-                        </div>
-                        <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 team">
-                            <div class="img-box">
-                                <img class="img-responsive" alt="Responsive Team Profiles" src="assets/img/team/Uthman_kolawole.jpeg" />
-                                <ul class="text-center">
-                                    <a href="#">
-                                        <li><i className="bx bxl-facebook"></i></li>
-                                    </a>
-                                    <a href="#">
-                                        <li><i className="bx bxl-twitter"></i></li>
-                                    </a>
-                                    <a href="#">
-                                        <li><i className="bx bxl-linkedin"></i></li>
-                                    </a>
-                                </ul>
-                            </div>
-                            <h1>Uthman Kolawole Bashir</h1>
-                            <h2>Pioneer Member</h2>
-                            <hr />
-                            <p class="text-justify">Uthman KOlawole Bashir is from Osun state with a BSc degree in Biochemistry, born on the 19th of June.</p>
-                        </div>
-                        <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 team">
-                            <div class="img-box">
-                                <img class="img-responsive" alt="Responsive Team Profiles" src="assets/img/team/Ibitanmi_Samson.jpeg" />
-                                <ul class="text-center">
-                                    <a href="#">
-                                        <li><i className="bx bxl-facebook"></i></li>
-                                    </a>
-                                    <a href="#">
-                                        <li><i className="bx bxl-twitter"></i></li>
-                                    </a>
-                                    <a href="#">
-                                        <li><i className="bx bxl-linkedin"></i></li>
-                                    </a>
-                                </ul>
-                            </div>
-                            <h1>Ibitanmi Samson</h1>
-                            <h2>Pioneer Member</h2>
-                            <hr />
-                            <p class="text-justify">Ibitanmi Samson is a Geotechnologist, hails from Ekiti State, CEO of Active Group of schools, born on the 9th of june 1992.</p>
+                            <p class="text-justify">Alofe Oluwatoba Andrew is the coordinator of this great foundation, with great passion and industrious efforts towards the operational efficacy of this organization, he is born on the 4th of May with a Bsc in Computer Science, currently working in Human Resources sector. He hails from Ekiti State</p>
                         </div>
                         <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 team">
                             <div class="img-box">
@@ -167,11 +67,153 @@ function Profile(props) {
                                     </a>
                                 </ul>
                             </div>
-                            <h1>Adeyanju Adedayo Iyanu</h1>
-                            <h2>Pioneer Member</h2>
+                            <h1 style={{fontWeight: 'bolder'}}>Adeyanju Adedayo Iyanu</h1>
+                            <h2>Assistant Coordinator</h2>
                             <hr />
                             <p class="text-justify">Adeyanju Iyanu is a citizen of Ekiti State, born on the 21st october he is self employed, and a statician. </p>
                         </div>
+
+                        <br/>
+                        <br/>
+                        <hr/>
+                        <br/>
+                        <h1 style={{textAlign:'center', width: '100%'}}>Pioneer Members:</h1>
+                        <br/>
+                        <hr/>
+                        <br/>
+                        <br/>
+                        
+                        <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 team">
+                            <div class="img-box">
+                                <img class="img-responsive" alt="Responsive Team Profiles" src="assets/img/team/Awolegon_Rachael.jpeg" />
+                                <ul class="text-center">
+                                    <a href="#">
+                                        <li><i className="bx bxl-facebook"></i></li>
+                                    </a>
+                                    <a href="#">
+                                        <li><i className="bx bxl-twitter"></i></li>
+                                    </a>
+                                    <a href="#">
+                                        <li><i className="bx bxl-linkedin"></i></li>
+                                    </a>
+                                </ul>
+                            </div>
+                            <h1 style={{fontWeight: 'bolder'}}>Awolegan Rachael Oluwatobiloba</h1>
+                            <h2>pioneer member</h2>
+                            <hr />
+                            <p class="text-justify">Miss Rachael Awolegan is a pioneer member of this great foundation, she is a fashion designer from ondo state, graduated from the university with a degree in Environmental health technology, born on the 10th of May.</p>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 team">
+                            <div class="img-box">
+                                <img class="img-responsive" alt="Responsive Team Profiles" src="assets/img/team/Ibitanmi_Samson.jpeg" />
+                                <ul class="text-center">
+                                    <a href="#">
+                                        <li><i className="bx bxl-facebook"></i></li>
+                                    </a>
+                                    <a href="#">
+                                        <li><i className="bx bxl-twitter"></i></li>
+                                    </a>
+                                    <a href="#">
+                                        <li><i className="bx bxl-linkedin"></i></li>
+                                    </a>
+                                </ul>
+                            </div>
+                            <h1 style={{fontWeight: 'bolder'}}>Ibitanmi Samson</h1>
+                            <h2>Pioneer Member</h2>
+                            <hr />
+                            <p class="text-justify">Ibitanmi Samson is a Geotechnologist, hails from Ekiti State, CEO of Active Group of schools, born on the 9th of june 1992.</p>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 team">
+                            <div class="img-box">
+                                <img class="img-responsive" alt="Responsive Team Profiles" src="assets/img/team/Olakitan_silifat.jpeg" />
+                                <ul class="text-center">
+                                    <a href="#">
+                                        <li><i className="bx bxl-facebook"></i></li>
+                                    </a>
+                                    <a href="#">
+                                        <li><i className="bx bxl-twitter"></i></li>
+                                    </a>
+                                    <a href="#">
+                                        <li><i className="bx bxl-linkedin"></i></li>
+                                    </a>
+                                </ul>
+                            </div>
+                            <h1 style={{fontWeight: 'bolder'}}>Ayodele Olakiitan Silifat</h1>
+                            <h2>Pioneer member</h2>
+                            <hr />
+                            <p class="text-justify">Olakiitan Silifat hails from Ekiti state, born on the 12th of June 1993</p>
+                        </div>
+                        <br/>
+                        <br/>
+                        <hr/>
+                        <br/>
+                        <h1 style={{textAlign:'center', width: '100%'}}>Members:</h1>
+                        <br/>
+                        <hr/>
+                        <br/>
+                        <br/>
+                        <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 team">
+                            <div class="img-box">
+                                <img class="img-responsive" alt="Responsive Team Profiles" src="assets/img/team/Akinyemi_Henry.jpeg" />
+                                <ul class="text-center">
+                                    <a href="#">
+                                        <li><i className="bx bxl-facebook"></i></li>
+                                    </a>
+                                    <a href="#">
+                                        <li><i className="bx bxl-twitter"></i></li>
+                                    </a>
+                                    <a href="#">
+                                        <li><i className="bx bxl-linkedin"></i></li>
+                                    </a>
+                                </ul>
+                            </div>
+                            <h1 style={{fontWeight: 'bolder'}}>Akinyemi Henry Olamide</h1>
+                            <h2> Member</h2>
+                            <hr />
+                            <p class="text-justify">Master Akinyemi Henry is a self employed citizen of Ekiti state with a degree in Business Education</p>
+                        </div>
+                       
+                        <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 team">
+                            <div class="img-box">
+                                <img class="img-responsive" alt="Responsive Team Profiles" src="assets/img/team/Uthman_kolawole.jpeg" />
+                                <ul class="text-center">
+                                    <a href="#">
+                                        <li><i className="bx bxl-facebook"></i></li>
+                                    </a>
+                                    <a href="#">
+                                        <li><i className="bx bxl-twitter"></i></li>
+                                    </a>
+                                    <a href="#">
+                                        <li><i className="bx bxl-linkedin"></i></li>
+                                    </a>
+                                </ul>
+                            </div>
+                            <h1 style={{fontWeight: 'bolder'}}>Uthman Kolawole Bashir</h1>
+                            <h2>Member</h2>
+                            <hr />
+                            <p class="text-justify">Uthman Kolawole Bashir is from Osun state with a BSc degree in Biochemistry, born on the 19th of June.</p>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 team">
+                            <div class="img-box">
+                                <img class="img-responsive" alt="Responsive Team Profiles" src="assets/img/team/Adejuwon_Taiwo.jpeg" />
+                                <ul class="text-center">
+                                    <a href="#">
+                                        <li><i className="bx bxl-facebook"></i></li>
+                                    </a>
+                                    <a href="#">
+                                        <li><i className="bx bxl-twitter"></i></li>
+                                    </a>
+                                    <a href="#">
+                                        <li><i className="bx bxl-linkedin"></i></li>
+                                    </a>
+                                </ul>
+                            </div>
+                            <h1 style={{fontWeight: 'bolder'}}>Adejuwon Taiwo Emmanuel </h1>
+                            <h2>Member</h2>
+                            <hr />
+                            <p class="text-justify">Adejuwon Taiwo is from Ondo state with a BSc degree in Banking and Finance, born on 28th of July.</p>
+                        </div>
+                       
                     </div>
                 </div>
             </div>
