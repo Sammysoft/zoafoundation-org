bbba07c1d0561a07f5aca1999c021a4dbddc1790
Samuel Dare-Owonibi <dareowonibisamuel@gmail.com>
2021-06-01 10:07:08 +0100

    more commits

--- src/Components/Pages/Team/team.js
+++ src/Components/Pages/Team/team.js
@@ -28,7 +28,7 @@ function Profile(props) {
                                 </ul>
                             </div>
                             <h1>Arijeloye Olakitan zacchaeus</h1>
-                            <h2>Founder</h2>
+                            <h2><b>Founder</b></h2>
                             <hr />
                             <p class="text-justify">Arijeloye Olakitan the founder of zoa foundation initiatives hails from Ibuji ondo state, born on the 10th of May, 1993 with a BSc degree in Statistics.</p>
                         </div>
@@ -48,7 +48,7 @@ function Profile(props) {
                                 </ul>
                             </div>
                             <h1>Alofe Oluwatoba Andrew</h1>
-                            <h2>Coordinator</h2>
+                            <h2><b>Coordinator</b></h2>
                             <hr />
                             <p class="text-justify">Alofe Oluwatoba Andrew is the coordinator of this great foundation, with great passion and industriious efforts towards the operational efficacy of this organization</p>
                         </div>
@@ -68,7 +68,7 @@ function Profile(props) {
                                 </ul>
                             </div>
                             <h1>Adeyanju Adedayo Iyanu</h1>
-                            <h2>Assistant Coordinator</h2>
+                            <h2><b>Assistant Coordinator</b></h2>
                             <hr />
                             <p class="text-justify">Adeyanju Iyanu is a citizen of Ekiti State, born on the 21st october he is self employed, and a statician. </p>
                         </div>
@@ -88,7 +88,7 @@ function Profile(props) {
                                 </ul>
                             </div>
                             <h1>Awolegan Rachael Oluwatobiloba</h1>
-                            <h2>pioneer member</h2>
+                            <h2><b>Pioneer Member</b></h2>
                             <hr />
                             <p class="text-justify">Miss Rachael Awolegan is a pioneer member of this great foundation, she is a fashion designer from ondo state, graduated from the university with a degree in Environmental health technology, born on the 10th of May 1996.</p>
                         </div>
@@ -109,9 +109,9 @@ function Profile(props) {
                                 </ul>
                             </div>
                             <h1>Ibisanmi Samson</h1>
-                            <h2>Pioneer Member</h2>
+                            <h2><b>Pioneer Member</b></h2>
                             <hr />
-                            <p class="text-justify">Ibisanmi Samson is a Geotechnologist, hails from Ekiti State, CEO of Active Group of schools, born on the 9th of june 1992.</p>
+                            <p class="text-justify">Ibisanmi Samson is a Geotechnologist, hails from Ekiti State, CEO of Active Group of schools, born on the 9th of june</p>
                         </div>
                         <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 team">
                             <div class="img-box">
@@ -129,9 +129,9 @@ function Profile(props) {
                                 </ul>
                             </div>
                             <h1>Ayodele Olakiitan Silifat</h1>
-                            <h2>Pioneer member</h2>
+                            <h2><b>Pioneer Member</b></h2>
                             <hr />
-                            <p class="text-justify">Olakiitan Silifat hails from Ekiti state, born on the 12th of June 1993</p>
+                            <p class="text-justify">Olakiitan Silifat hails from Ekiti state, born on the 12th of June</p>
                         </div>
                         <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 team">
                             <div class="img-box">
@@ -149,7 +149,7 @@ function Profile(props) {
                                 </ul>
                             </div>
                             <h1>Dare-Owonibi Samuel</h1>
-                            <h2>Pioneer member</h2>
+                            <h2><b>Pioneer Member</b></h2>
                             <hr />
                             <p class="text-justify">Dare-Owonibi Samuel is an undergraduate of computer Science from FUTA born on the 10th of December.</p>
                         </div>
@@ -169,7 +169,7 @@ function Profile(props) {
                                 </ul>
                             </div>
                             <h1>Uthman Kolawole Bashiru</h1>
-                            <h2>Member</h2>
+                            <h2><b>Member</b></h2>
                             <hr />
                             <p class="text-justify">Uthman Kolawole Bashiru is from Osun state with a BSc degree in Biochemistry, born on the 19th of June.</p>
                         </div>
@@ -189,7 +189,7 @@ function Profile(props) {
                                 </ul>
                             </div>
                             <h1>Akinyemi Henry Olamide</h1>
-                            <h2>Member</h2>
+                            <h2><b>Member</b></h2>
                             <hr />
                             <p class="text-justify">Master Akinyemi Henry is a self employed citizen of Ekiti state with a degree in Business Education</p>
                         </div>
@@ -209,7 +209,7 @@ function Profile(props) {
                                 </ul>
                             </div>
                             <h1>Miss Oluwatomisin</h1>
-                            <h2>Member</h2>
+                            <h2><b>Member</b></h2>
                             <hr />
                             <p class="text-justify">Miss Oluwatomisin, is a student born on the 26th of May from Osun State</p>
                         </div>
@@ -229,9 +229,29 @@ function Profile(props) {
                                 </ul>
                             </div>
                             <h1>Orimoloye Oluwatomisin</h1>
-                            <h2>Member</h2>
+                            <h2><b>Member</b></h2>
                             <hr />
-                            <p class="text-justify">Orimoloye Oluwatomisin, studied BAM, born on the 6th of Match from Lagos</p>
+                            <p class="text-justify">Orimoloye Oluwatomisin, studied BAM, born on the 6th of March from Lagos</p>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 team">
+                            <div class="img-box">
+                                <img class="img-responsive" alt="Responsive Team Profiles" src="assets/img/team/Taiwo.jpeg" />
+                                <ul class="text-center">
+                                    <a href="#">
+                                        <li><i className="bx bxl-facebook"></i></li>
+                                    </a>
+                                    <a href="#">
+                                        <li><i className="bx bxl-twitter"></i></li>
+                                    </a>
+                                    <a href="#">
+                                        <li><i className="bx bxl-linkedin"></i></li>
+                                    </a>
+                                </ul>
+                            </div>
+                            <h1>Adejuwon Taiwo Emmanuel</h1>
+                            <h2><b>Member</b></h2>
+                            <hr />
+                            <p class="text-justify">Adejuwon Taiwo Emmanuel, is a student from ondo state studying banking and finance, born on 28th of July.</p>
                         </div>
                         <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 team">
                             <div class="img-box">
@@ -249,13 +269,16 @@ function Profile(props) {
                                 </ul>
                             </div>
                             <h1> Oluwaseun Onaopemipo</h1>
-                            <h2>Member</h2>
+                            <h2><b>Member</b></h2>
                             <hr />
-                            <p class="text-justify"> Oluwaseun Onaopemipo, is a student, born on the 2oth of June from Lagos</p>
+                            <p class="text-justify"> Oluwaseun Onaopemipo, is a student, born on the 20th of June from Lagos</p>
                         </div>
+                        
                     </div>
+                    
                 </div>
             </div>
+            
         </div>
     );
 }
